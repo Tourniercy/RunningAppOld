318a369970e1c304baf4003403d75254edd5725b
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2019-12-03 16:47:26 +0100

    Update app

--- App/view/Newacc.js
+++ App/view/Newacc.js
@@ -1,22 +1,43 @@
 import React,{Component} from 'react';
-import {View, Text, Image, StyleSheet, Platform, StatusBar} from 'react-native';
+import {View, Text, Image, StyleSheet, Platform, StatusBar, Button} from 'react-native';
+import { Input } from 'react-native-elements';
 export class FormAcc extends Component {
     render() {
         return (
             <View style={styles.img}>
                 <Image
-                    style={{width: 50, height: 50}}
+                    style={{width: 100, height: 100}}
                     source={require('../src/img/Logo.png')}
                 />
-                <Text>RUNNING</Text>
+                <Text style={{fontSize:40, paddingTop: 20,color: '#2C5077'}}>R U N N I N G</Text>
+                <Input inputStyle={{marginTop:20}}
+                    placeholder='Adresse mail'
+                />
+                <Input inputStyle={{marginTop:20}}
+                    placeholder='Mot de passe'
+                />
+                <Text style={{fontSize:15, paddingTop: 20,color: '#2C5077',textAlign:'right',alignSelf: 'stretch'}}>Mot de passe oublié ?</Text>
+                <View style={{ flexDirection: 'row' }}>
+                <Button style={{margin:15}}
+                    title="Connexion"
+                    type="outline"
+                    color="#2C5077"
+                />
+                <Button
+                    style={{margin:15}}
+                    title="Inscription"
+                    type="outline"
+                    color="#2C5077"
+                />
+                </View>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
     img: {
-        paddingTop : '10px',
+        paddingTop : 50,
         alignItems: 'center',
-        flex: 2,
+        flex: 6,
     },
 });
