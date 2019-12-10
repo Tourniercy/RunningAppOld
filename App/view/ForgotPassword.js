d81cca1088a0123e8646c32a447599210787f0a5
Lilian MAHEVAS <lilian.mahevas@ynov.com>
2019-12-10 13:01:31 +0100

    message + champ

--- App/view/ForgotPassword.js
+++ App/view/ForgotPassword.js
@@ -1,15 +1,29 @@
 // View if you forget your password
 
 import React,{Component} from 'react';
-import { StyleSheet, View, Text } from 'react-native'
+import {StyleSheet, View, Text, TextInput} from 'react-native'
 
 class ForgotPassword extends Component {
+
+    state = {
+        email: ''
+    }
+    handleEmail = (text) => {
+        this.setState({ email: text })
+    }
+
 
     render() {
         const { navigation } = this.props;
         return (
             <View style={styles.main_container}>
-                <Text>Login: {JSON.stringify(navigation.getParam('Login', 'default value'))}</Text>
+                <Text style={styles.text}>Entrez votre adresse mail, et vous recevrez un mail vous permettant de changer votre mot de passe.</Text>
+                <TextInput
+                    placeholder="Email"
+                    onChangeText={this.handleEmail}
+                    style={styles.textInput}
+
+                />
             </View>
         )
     }
@@ -18,6 +32,21 @@ class ForgotPassword extends Component {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1,
+        alignContent: 'center'
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 30
+    },
+    textInput: {
+        width : 300,
+        marginTop:25,
+        margin: 25,
+        alignSelf: 'stretch',
+        padding: 7,
+        borderBottomColor:'grey',
+        borderBottomWidth: 1,     // Add this to specify bottom border thickness
+        fontSize: 20
     }
 })
 
