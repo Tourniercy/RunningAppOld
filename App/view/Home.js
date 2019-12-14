876c9922db4c526a4abbf1c6725056912bb1e849
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2019-12-14 22:11:25 +0100

    Update home.js

--- App/view/Home.js
+++ App/view/Home.js
@@ -2,9 +2,14 @@
 import { Text, View } from 'react-native';
 import React, { Component } from 'react';
 import MapView from "react-native-maps";
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import * as geolib from 'geolib';
+
 
 
 export default class App extends Component {
+
     constructor(props) {
         super(props);
 
@@ -14,43 +19,81 @@ export default class App extends Component {
             error: null,
             prevLatitude: null,
             prevLongitude: null,
+            location: null,
+            distance: null,
         };
     }
 
     componentDidMount() {
-        this.watchId = navigator.geolocation.watchPosition(
-            (position) => {
-                this.setState({
-                    latitude: position.coords.latitude,
-                    longitude: position.coords.longitude,
-                    prevLatitude : position.coords.latitude,
-                    prevLongitude : position.coords.longitude,
-                    error: null,
-                });
-            },
-            (error) => this.setState({ error: error.message }),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 0, distanceFilter: 1},
+        this.findCurrentLocationAsync().then(() => {
+            if (this.state.location)  {
+                this.watchId = navigator.geolocation.watchPosition(
+                    (position) => {
+                        this.setState({
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                            error: null,
+                        });
+                        if (this.state.prevLatitude)  {
+                            this.setState({
+                                distance:  geolib.getDistance(
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                                    { latitude: this.state.prevLatitude, longitude: this.state.prevLongitude }
+                                ),
+                                prevLatitude : position.coords.latitude,
+                                prevLongitude : position.coords.longitude,
+                            });
+                        } else {
+                            this.setState({
+                                prevLatitude : this.state.latitude,
+                                prevLongitude : this.state.longitude,
+                                distance:  geolib.getDistance(
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude }
+                                )
+                            });
+                        }
 
-        );
+                    },
+                    (error) => this.setState({ error: error.message }),
+                    { enableHighAccuracy: true, timeout: 20000, maximumAge: 0, distanceFilter: 1},
+
+                );
+            }
+        });
     }
 
     componentWillUnmount() {
         navigator.geolocation.clearWatch(this.watchId);
     }
+    findCurrentLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
 
+            if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied'
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ location });
+    };
     render() {
         let text = 'Waiting..';
         let latitude = 0;
         let longitude = 0;
+        let distance = 0;
         if (this.state.error) {
             text = this.state.error;
         } else if (this.state.latitude) {
             latitude = (this.state.latitude);
             longitude = (this.state.longitude);
+            distance = (this.state.distance);
         }
         return (
             <View style={{flex:1}}>
                 <MapView
+                    showsUserLocation={ true }
                     style={{
                         flex: 1
                     }}
@@ -61,13 +104,8 @@ export default class App extends Component {
                         longitudeDelta: 0.02
                     }}
                 >
-                    <MapView.Marker
-                        coordinate={{latitude: latitude,
-                            longitude: longitude}}
-                        title={"title"}
-                        description={"description"}
-                    />
                 </MapView>
+                <Text>{distance}</Text>
             </View>
         );
     }
