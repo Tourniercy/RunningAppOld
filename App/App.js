b5c726c23208d93331058ced2d17650e8a7ee5f8
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2019-12-14 01:19:27 +0100

    Ajout de home.js avec la maps et un pointer qui se deplace en fonction de la position du téléphone

--- App/App.js
+++ App/App.js
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
 import {Platform, StatusBar, StyleSheet, SafeAreaView} from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import Container from './Navigation';
-import Inscription from './view/Inscription';
+import Home from './view/Home';
 
 export default class YourApp extends Component {
     render() {
         return (
             <SafeAreaView style={styles.droidSafeArea}>
-                <Container />
+                <Home />
             </SafeAreaView>
 
         );
--- App/view/Home.js
+++ App/view/Home.js
@@ -1,25 +1,74 @@
-// View/Home.js
 
-import React,{Component} from 'react';
-import { StyleSheet, View, Text } from 'react-native'
+import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import MapView from "react-native-maps";
 
-class Home extends Component {
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            latitude: null,
+            longitude: null,
+            error: null,
+            prevLatitude: null,
+            prevLongitude: null,
+        };
+    }
+
+    componentDidMount() {
+        this.watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    prevLatitude : position.coords.latitude,
+                    prevLongitude : position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 0, distanceFilter: 1},
+
+        );
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId);
+    }
 
     render() {
-        const { navigation } = this.props;
+        let text = 'Waiting..';
+        let latitude = 0;
+        let longitude = 0;
+        if (this.state.error) {
+            text = this.state.error;
+        } else if (this.state.latitude) {
+            latitude = (this.state.latitude);
+            longitude = (this.state.longitude);
+        }
         return (
-            <View style={styles.main_container}>
-                <Text>Login: {JSON.stringify(navigation.getParam('Login', 'default value'))}</Text>
-                <Text>Password: {JSON.stringify(navigation.getParam('Password', 'default value'))}</Text>
+            <View style={{flex:1}}>
+                <MapView
+                    style={{
+                        flex: 1
+                    }}
+                    region={{
+                        latitude: latitude,
+                        longitude: longitude,
+                        latitudeDelta: 0.02,
+                        longitudeDelta: 0.02
+                    }}
+                >
+                    <MapView.Marker
+                        coordinate={{latitude: latitude,
+                            longitude: longitude}}
+                        title={"title"}
+                        description={"description"}
+                    />
+                </MapView>
             </View>
-        )
+        );
     }
 }
-
-const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-    }
-})
-
-export default Home
