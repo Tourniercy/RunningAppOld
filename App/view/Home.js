26e735fcc94ed738c885b6435239a1fad4019239
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2019-12-16 10:21:00 +0100

    Update Home.js

--- App/view/Home.js
+++ App/view/Home.js
@@ -1,7 +1,7 @@
 
 import { Text, View } from 'react-native';
 import React, { Component } from 'react';
-import MapView from "react-native-maps";
+import MapView, { Polyline,Polygon } from "react-native-maps";
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
 import * as geolib from 'geolib';
@@ -21,6 +21,12 @@ export default class App extends Component {
             prevLongitude: null,
             location: null,
             distance: null,
+            routeCoordinates : null,
+            totalDistance : null,
+            coordinate: ({
+                latitude: null,
+                longitude: null
+            })
         };
     }
 
@@ -42,6 +48,11 @@ export default class App extends Component {
                                 ),
                                 prevLatitude : position.coords.latitude,
                                 prevLongitude : position.coords.longitude,
+                                totalDistance : geolib.getDistance(
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                                    { latitude: this.state.prevLatitude, longitude: this.state.prevLongitude }
+                                ) + this.state.totalDistance,
+                                routeCoordinates: this.state.routeCoordinates.concat([{latitude: position.coords.latitude, longitude: position.coords.longitude}]),
                             });
                         } else {
                             this.setState({
@@ -50,7 +61,13 @@ export default class App extends Component {
                                 distance:  geolib.getDistance(
                                     { latitude: position.coords.latitude, longitude: position.coords.longitude },
                                     { latitude: position.coords.latitude, longitude: position.coords.longitude }
-                                )
+                                ),
+                                totalDistance : geolib.getDistance(
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude },
+                                    { latitude: position.coords.latitude, longitude: position.coords.longitude }
+                                ),
+                                routeCoordinates: ([{latitude: position.coords.latitude, longitude: position.coords.longitude}]),
+
                             });
                         }
 
@@ -76,6 +93,7 @@ export default class App extends Component {
         }
 
         let location = await Location.getCurrentPositionAsync({});
+            console.log(location);
         this.setState({ location });
     };
     render() {
@@ -83,12 +101,18 @@ export default class App extends Component {
         let latitude = 0;
         let longitude = 0;
         let distance = 0;
+        let totalDistance = 0;
+        let routeCoordinates= [];
+
         if (this.state.error) {
             text = this.state.error;
         } else if (this.state.latitude) {
             latitude = (this.state.latitude);
             longitude = (this.state.longitude);
             distance = (this.state.distance);
+            totalDistance = (this.state.totalDistance);
+            routeCoordinates = (this.state.routeCoordinates);
+
         }
         return (
             <View style={{flex:1}}>
@@ -104,7 +128,22 @@ export default class App extends Component {
                         longitudeDelta: 0.02
                     }}
                 >
+                    <Polyline
+                        coordinates={routeCoordinates}
+                        strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+                        strokeColors={[
+                            '#7F0000',
+                            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
+                            '#B24112',
+                            '#E5845C',
+                            '#238C23',
+                            '#7F0000'
+                        ]}
+                        strokeWidth={6}
+                    />
                 </MapView>
+
+
                 <Text>{distance}</Text>
             </View>
         );
