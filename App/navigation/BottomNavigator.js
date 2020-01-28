1cc39c6e23afaae76c2780bd11eea70368d01772
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2020-01-28 12:07:44 +0100

    Bottomnav

--- App/navigation/BottomNavigator.js
+++ App/navigation/BottomNavigator.js
@@ -16,7 +16,7 @@ class HomeScreen extends React.Component {
     render() {
         return(
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text> This is my Home screen </Text>
+                <Text> This is my Home screenn </Text>
             </View>
         );
     }
