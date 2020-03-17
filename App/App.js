8c844d8a270b47f65d550a87084f1d533631a123
cyril.tournier@ynov.com <cyril.tournier@ynov.com>
2020-03-17 16:02:15 +0100

    Login token

--- App/App.js
+++ App/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from 'react';
-import {Platform, StatusBar, StyleSheet, SafeAreaView} from 'react-native';
-import Container from './navigation/Navigation';
-import BottomNavigation from './navigation/BottomNavigator'
-import Home from './view/Home';
-
+import React, {Component} from "react";
+import {Platform, StatusBar, StyleSheet, SafeAreaView, AppRegistry} from 'react-native';
+import { createRootNavigator } from "./navigation/Navigation";
+import { NavigationContainer } from '@react-navigation/native';
+import { isSignedIn } from "./auth/Auth";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            signedIn: false,
+            checkedSignIn: false
+        };
+    }
+
+    componentDidMount() {
+        isSignedIn()
+            .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+            .catch(err => alert("An error occurred"));
+    }
+
     render() {
+        const { checkedSignIn, signedIn } = this.state;
+
+        // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+        if (!checkedSignIn) {
+            return null;
+        }
+
+        const Layout = createRootNavigator(signedIn);
         return (
             <SafeAreaView style={styles.droidSafeArea}>
-                    <BottomNavigation />
+                    <Layout />
             </SafeAreaView>
+
         );
     }
 }
@@ -21,5 +44,3 @@ const styles = StyleSheet.create({
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
     },
 });
-
-
